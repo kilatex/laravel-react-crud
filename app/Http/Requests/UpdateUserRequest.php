0b40e9a1185a5e6a1ff85290f86a369eeef1193f
Santiago <santiagodmaldon@gmail.com>
2024-08-10 21:22:11 -0500

    users.jsx

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -26,7 +26,7 @@ class UpdateUserRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:55',
-            'email' => 'required|email|unique:users,email,'.$this->id,
+            'email' => 'required|email|unique:users,email,' . $this->id,
             'password' => [
                 'confirmed',
                 Password::min(8)
